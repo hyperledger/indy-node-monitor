2d4d820e2cb77a228d82186e2d0428f86a0a6db0
Wade Barnes <wade.barnes@shaw.ca>
2020-07-31 08:06:24 -0700

    Fix white space
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- fetch-validator-status/run.sh
+++ fetch-validator-status/run.sh
@@ -9,7 +9,7 @@ fi
 
 docker build -t fetch_status .
 ${terminalEmu} docker run --rm -ti \
-	-e "GENESIS_PATH=${GENESIS_PATH}" \
-	-e "GENESIS_URL=${GENESIS_URL}" \
-	-e "SEED=${SEED}" \
-	fetch_status "$@"
+    -e "GENESIS_PATH=${GENESIS_PATH}" \
+    -e "GENESIS_URL=${GENESIS_URL}" \
+    -e "SEED=${SEED}" \
+    fetch_status "$@"
