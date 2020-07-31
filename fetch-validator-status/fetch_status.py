ec2bb39295bd5499e8be0dd322641c8bc33e596e
Wade Barnes <wade.barnes@shaw.ca>
2020-07-31 10:42:58 -0700

    Add full response to anonymous request output.
    Signed-off-by: Wade Barnes <wade.barnes@shaw.ca>

--- fetch-validator-status/fetch_status.py
+++ fetch-validator-status/fetch_status.py
@@ -57,12 +57,13 @@ async def fetch_status(genesis_path: str, ident: DidKey = None):
     for node, val in response.items():
         entry = {"name": node}
         try:
-            jsval = json.loads(val)
+            jsval = json.loads(val)            
             if "REPLY" in jsval["op"]:
                 if ident:
                     entry["response"] = jsval["result"]["data"]
                 else:
                     entry["response"] = "ok"
+                    entry["full_response"] = jsval
             else:
                 if "reason" in jsval:
                     entry["error"] = jsval["reason"]
