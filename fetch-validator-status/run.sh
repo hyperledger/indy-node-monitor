02ad9cc9eecfc5bf0a49169d410970cd3e2832ad
Stephen Curran <swcurran@gmail.com>
2020-08-24 09:48:23 -0700

    Added a way to not use the -it mode for docker easily by setting the CM env variable on run #13
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- fetch-validator-status/run.sh
+++ fetch-validator-status/run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -x
+
 export MSYS_NO_PATHCONV=1
 
 # Running on Windows?
@@ -7,8 +9,16 @@ if [[ "$OSTYPE" == "msys" ]]; then
   terminalEmu="winpty"
 fi
 
+# CM is for "cron mode" so Docker is run the right way for cron
+# Use "CM=1 run.sh <args>..." to run from a fron job to run Docker in non-interactive mode
+if [ -z "${CM+x}" ]; then
+  export DOCKER_INTERACTIVE="-it"
+else
+  export DOCKER_INTERACTIVE=""
+fi
+
 docker build -t fetch_status . > /dev/null 2>&1
-${terminalEmu} docker run --rm -ti \
+${terminalEmu} docker run --rm ${DOCKER_INTERACTIVE} \
     -e "GENESIS_PATH=${GENESIS_PATH}" \
     -e "GENESIS_URL=${GENESIS_URL}" \
     -e "SEED=${SEED}" \
