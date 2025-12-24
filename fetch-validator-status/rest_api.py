b951721d07ccf80b75307ab2e2af68fc9f35b8ef
Stephen Curran <swcurran@gmail.com>
2025-12-24 11:24:03 -0800

    Update after move to new GitHub org
    Signed-off-by: Stephen Curran <swcurran@gmail.com>

--- fetch-validator-status/rest_api.py
+++ fetch-validator-status/rest_api.py
@@ -15,7 +15,7 @@ from fetch_status import FetchStatus, NodeNotFound
 from plugin_collection import PluginCollection
 
 APP_NAME='Hyperledger Indy Node Monitor REST API'
-APP_DESCRIPTION='https://github.com/hyperledger/indy-node-monitor'
+APP_DESCRIPTION='https://github.com/hyperledger-indy/indy-node-monitor'
 APP_VERSION='0.0.0'
 
 # https://fastapi.tiangolo.com/tutorial/metadata/
